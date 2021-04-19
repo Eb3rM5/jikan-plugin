1f26169f8f6461399df94741400621420ce059d0
Éber Mainardes <mainardes.eber@gmail.com>
2021-04-19 07:48:14 -0300

    Adding utility methods to TimeRange record and making TimeCard based on LocalDate

--- src/main/java/dev/mainardes/app/jikan/entity/TimeCard.java
+++ src/main/java/dev/mainardes/app/jikan/entity/TimeCard.java
@@ -1,22 +1,49 @@
 package dev.mainardes.app.jikan.entity;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import dev.mainardes.app.jikan.plugin.WatchTimePlugin;
+import dev.mainardes.app.jikan.util.JikanUtil;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class TimeCard {
 
-    private final List<TimeRange> ranges;
+    private final LocalDate date;
+    private final TimeRange[] ranges;
 
-    public TimeCard(List<TimeRange> ranges){
+    public TimeCard(LocalDate date, TimeRange[] ranges){
+        this.date = date;
         this.ranges = ranges;
     }
 
-    public List<TimeRange> getRanges() {
+    public TimeCard(TimeRange[] ranges){
+        this(null, ranges);
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public TimeRange[] getRanges() {
         return ranges;
     }
 
-    public static record TimeRange(LocalDateTime start,
-                                   LocalDateTime end){
+    public static record TimeRange(LocalTime start,
+                                   LocalTime end){
+
+        public boolean isEndHit(LocalTime time, JikanUser user, WatchTimePlugin<?> plugin){
+            var limit = end.plus(plugin.getTimePointLimit(user));
+            return JikanUtil.isBetween(time, end, limit);
+        }
+
+        public boolean isStartHit(LocalTime time, JikanUser user, WatchTimePlugin<?> plugin){
+            var limit = start.plus(plugin.getTimePointLimit(user));
+            return JikanUtil.isBetween(time, start, limit);
+        }
+
+        public boolean isBetween(LocalTime time){
+            return JikanUtil.isBetween(time, start, end);
+        }
 
     }
 
